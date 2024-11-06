689bc82158b40091aa45461dc81b0d0362f4be54
MedoAlshafei <elshafey009@gmail.com>
2024-11-06 23:47:56 +0200

    half of home view design

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weathers/color.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -11,9 +12,90 @@ class HomeView extends StatelessWidget {
         leading: const Icon(
           Icons.menu_rounded,
         ),
-        title: const Text('San Francisco'),
+        title: const Center(child: Text('San Francisco')),
       ),
-      body: null,
+      body: Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 70),
+              child: Image.asset(
+                'images/clear.png',
+                fit: BoxFit.fill,
+                width: double.infinity,
+                height: 400,
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32),
+            child: Row(
+              children: [
+                Text(
+                  '52°',
+                  style: TextStyle(
+                    color: darkpurple,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 70,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Sunny',
+                        style: TextStyle(
+                          color: darkpurple,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'H:63°|L:51',
+                            style: TextStyle(
+                              color: darkpurple,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            child: Row(
+              children: [
+                Card(
+                  elevation: 0,
+                  color: whiteBlue,
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Today',
+                        style: TextStyle(
+                          color: white,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Image.asset('images/clear.png'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
